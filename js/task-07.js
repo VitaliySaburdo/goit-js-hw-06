a16c479d8af1d6497505ce4d963996afebb83bd2
VitaliySaburdo <vsaburdo@gmail.com>
2022-10-26 16:00:22 +0300

    fix

--- js/task-07.js
+++ js/task-07.js
@@ -5,7 +5,9 @@
 const input = document.querySelector("#font-size-control");
 const textOutput = document.querySelector("#text");
 
-input.addEventListener("change", changeFontSize);
+console.dir(input);
+
+input.addEventListener("input", changeFontSize);
 
 function changeFontSize(event) {
   textOutput.style.fontSize = event.currentTarget.value + "px";
