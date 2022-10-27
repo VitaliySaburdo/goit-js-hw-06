990744d98aa4fe2a8a798f1a348f10a5dcd65943
VitaliySaburdo <vsaburdo@gmail.com>
2022-10-27 09:55:55 +0300

    10 task

--- js/task-10.js
+++ js/task-10.js
@@ -3,26 +3,26 @@
  */
 const render = document.querySelector("[data-create]");
 const destroy = document.querySelector("[data-destroy]");
-const boxes = document.getElementById("boxes");
-render.addEventListener("click", getAmount);
+const input = document.querySelector("input");
+const boxes = document.querySelector("#boxes");
+
+render.addEventListener("click", createBoxes);
 destroy.addEventListener("click", destroyBoxes);
 
-function getAmount() {
-  const amount = document.querySelector("#controls input").value;
-  createBoxes(amount);
-}
-
-function createBoxes(amount) {
-  const basicSize = 30;
-  const fragment = document.createDocumentFragment();
-  console.log(fragment);
-  for (let i = 0; i < amount; i++) {
-    const size = basicSize + i * 10;
+let size = 30;
+function createBoxes() {
+  const counter = input.value;
+  const markup = [];
+  for (let i = 1; i <= counter; i += 1) {
+    size += 10;
     const div = document.createElement("div");
-    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}`;
-    fragment.appendChild(div);
+    div.style.width = size + "px";
+    div.style.height = size + "px";
+    div.style.backgroundColor = getRandomHexColor();
+    markup.push(div);
+    console.log(markup);
   }
-  boxes.appendChild(fragment);
+  boxes.append(...markup);
 }
 
 function destroyBoxes() {
